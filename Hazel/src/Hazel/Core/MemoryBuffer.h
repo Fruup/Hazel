8fb72bd463193a08b28c5a56274048cdc80088c8
Leon Scherer <scherer-leon@web.de>
2020-03-28 02:31:16 +0100

    Memory buffer system for easy (de-)serialization of class members

--- Hazel/src/Hazel/Core/MemoryBuffer.h
+++ Hazel/src/Hazel/Core/MemoryBuffer.h
@@ -1,55 +1,56 @@
 #pragma once
 
+#include <string>
+#include <array>
+#include <assert.h>
+
 namespace Hazel
 {
-	template <size_t Size = 32>
+	using MemoryType = uint8_t;
+	using StringLenType = uint32_t;
+
+
+	template <uint32_t MaxSize>
 	class MemoryBuffer
 	{
 	public:
 		MemoryBuffer() = default;
 
-		template <size_t SizeB>
-		inline MemoryBuffer(const MemoryBuffer<SizeB>& b) { Push(b.m_Stream, b.m_Pointer); }
+		template <uint32_t _>
+		inline MemoryBuffer(const MemoryBuffer<_>& buffer) { Push(buffer); }
 
-		template <typename T>
-		inline void Push(const T& data) { Push(&data, sizeof(T)); }
 
-		void Push(void* data, size_t size)
+		// Push data to the buffer
+		void Push(void* data, uint32_t size)
 		{
-			HZ_CORE_ASSERT(GetHeadroom() >= size, "Packet size not sufficient!");
+			assert(GetHeadroom() >= size);
+
 			memcpy(m_Stream + m_Pointer, data, size);
 			m_Pointer += size;
 		}
 
-		void Push(const char* string)
+		inline void Push(const std::string& string)
 		{
-			const size_t N = strlen(string);
-			Push(N); Push(string, N + 1);
+			StringLenType n = (StringLenType)(string.length() + 1);
+			Push(n);
+			Push((void*)string.data(), n);
 		}
 
-		inline size_t GetHeadroom() const { return Size - m_Pointer; }
-		inline unsigned char* GetStream() { return m_Stream; }
-		inline size_t GetPointer() const { return m_Pointer; }
+		template <typename T>
+		inline void Push(const T& data) { Push((void*)&data, sizeof(T)); }
+
+		template <uint32_t SizeB>
+		inline void Push(const MemoryBuffer<SizeB>& buffer) { Push(buffer.GetStream(), buffer.GetPointer()); }
+
+
+		// Getters
+		inline uint32_t GetHeadroom() const { return MaxSize - m_Pointer; }
+		inline const MemoryType* GetStream() const { return m_Stream; }
+		inline uint32_t GetPointer() const { return m_Pointer; }
+		inline uint32_t GetMaxSize() const { return MaxSize; }
 
 	private:
-		unsigned char m_Stream[Size];
-		size_t m_Pointer = 0;
-	};
-
-	template <size_t Size>
-	class Serializable
-	{
-	public:
-		inline const MemoryBuffer<Size>& Serialize() { SerializeImpl(m_Buffer); return m_Buffer; }
-		inline void Deserialize() { DeserializeImpl(m_Buffer); }
-
-	protected:
-		Serializable() = default;
-
-		virtual void SerializeImpl(MemoryBuffer<Size>& buffer) = 0;
-		virtual void DeserializeImpl(MemoryBuffer<Size>& buffer) = 0;
-
-	private:
-		MemoryBuffer<Size> m_Buffer;
+		MemoryType m_Stream[MaxSize];
+		uint32_t m_Pointer = 0;
 	};
 }
