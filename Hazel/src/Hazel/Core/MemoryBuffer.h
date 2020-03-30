5f7ff6fceec01661a570d667083e25113cc3319f
Leon Scherer <scherer-leon@web.de>
2020-03-30 15:23:03 +0200

    Memory buffer system improvements

--- Hazel/src/Hazel/Core/MemoryBuffer.h
+++ Hazel/src/Hazel/Core/MemoryBuffer.h
@@ -9,6 +9,9 @@ namespace Hazel
 	using MemoryType = uint8_t;
 	using StringLenType = uint32_t;
 
+	template <uint32_t> class Serializable;
+
+	// ---------------------------------
 
 	template <uint32_t MaxSize>
 	class MemoryBuffer
@@ -18,6 +21,8 @@ namespace Hazel
 
 		template <uint32_t _>
 		inline MemoryBuffer(const MemoryBuffer<_>& buffer) { Push(buffer); }
+
+		inline MemoryBuffer(void* data, uint32_t size) { Push(data, size); }
 
 
 		// Push data to the buffer
@@ -36,11 +41,37 @@ namespace Hazel
 			Push((void*)string.data(), n);
 		}
 
-		template <typename T>
-		inline void Push(const T& data) { Push((void*)&data, sizeof(T)); }
+#define MEMORY_BUFFER_CONSTR(type)\
+		inline void Push(const type& data) { Push((void*)&data, sizeof(type)); }
 
-		template <uint32_t SizeB>
-		inline void Push(const MemoryBuffer<SizeB>& buffer) { Push(buffer.GetStream(), buffer.GetPointer()); }
+#define MEMORY_BUFFER_CONSTR_SIGNED_UNSIGNED(type)\
+		inline void Push(const signed type& data) { Push((void*)&data, sizeof(type)); }\
+		inline void Push(const unsigned type& data) { Push((void*)&data, sizeof(type)); }
+
+		MEMORY_BUFFER_CONSTR_SIGNED_UNSIGNED(char)
+		MEMORY_BUFFER_CONSTR_SIGNED_UNSIGNED(short)
+		MEMORY_BUFFER_CONSTR_SIGNED_UNSIGNED(int)
+		MEMORY_BUFFER_CONSTR_SIGNED_UNSIGNED(long)
+		MEMORY_BUFFER_CONSTR_SIGNED_UNSIGNED(long long)
+		MEMORY_BUFFER_CONSTR(float)
+		MEMORY_BUFFER_CONSTR(double)
+		MEMORY_BUFFER_CONSTR(long double)
+
+#undef MEMORY_BUFFER_CONSTR
+#undef MEMORY_BUFFER_CONSTR_SIGNED_UNSIGNED
+
+		/*template <typename T>
+		inline void Push(const T& data)
+		{
+			HZ_CORE_ASSERT(std::is_fundamental<T>::value, "This function is only safe for primitive data types");
+			Push((void*)&data, sizeof(T));
+		}*/
+
+		template <uint32_t _>
+		inline void Push(const MemoryBuffer<_>& buffer) { Push(buffer.GetStream(), buffer.GetPointer()); }
+
+		template <uint32_t _>
+		inline void Push(const Serializable<_>& s);
 
 
 		// Getters
@@ -53,4 +84,16 @@ namespace Hazel
 		MemoryType m_Stream[MaxSize];
 		uint32_t m_Pointer = 0;
 	};
+}
+
+#include "Hazel/Core/Serialize.h"
+
+namespace Hazel
+{
+	template <uint32_t MaxSize>
+	template <uint32_t _>
+	inline void MemoryBuffer<MaxSize>::Push(const Serializable<_>& s)
+	{
+		s.Serialize(*this);
+	}
 }
--- Hazel/src/Hazel/Core/Serialize.h
+++ Hazel/src/Hazel/Core/Serialize.h
@@ -3,64 +3,36 @@
 #include "Hazel.h"
 #include "Hazel/Core/MemoryBuffer.h"
 
-#define HZ_COMPILE_ASSERT(x) int __assertDummy[(int)(x)]
-
 namespace Hazel
 {
 	enum class MemoryChunkType : uint32_t
 	{
-		Unknown,
-		Char, Short, Int, Long, LongLong,
-		UChar, UShort, UInt, ULong, ULongLong,
-		Float, Double, LongDouble,
-		PtrDiff,
-		StdString
+		Unknown, Fundamental, StdString
 	};
 
 	// -----------------------------
 
 	struct DataBlock
 	{
-#define DATA_BLOCK_CONSTR_SIGNED_UNSIGNED(Typename, DataType)\
-		inline DataBlock(const signed Typename& data)\
-			: DataBlock((void*)&data, sizeof(Typename), MemoryChunkType::##DataType) {}\
-		inline DataBlock(const unsigned Typename& data)\
-			: DataBlock((void*)&data, sizeof(Typename), MemoryChunkType::U##DataType) {}
-
-#define DATA_BLOCK_CONSTR(Typename, DataType)\
-		inline DataBlock(const Typename& data)\
-			: DataBlock((void*)&data, sizeof(Typename), MemoryChunkType::##DataType) {}
-
-
 		DataBlock() = default;
 		DataBlock(DataBlock&&) = default;
 
-		// Constructors
-		DATA_BLOCK_CONSTR_SIGNED_UNSIGNED(char, Char)
-		DATA_BLOCK_CONSTR_SIGNED_UNSIGNED(short, Short)
-		DATA_BLOCK_CONSTR_SIGNED_UNSIGNED(int, Int)
-		DATA_BLOCK_CONSTR_SIGNED_UNSIGNED(long, Long)
-		DATA_BLOCK_CONSTR_SIGNED_UNSIGNED(long long, LongLong)
-
-		DATA_BLOCK_CONSTR(float, Float)
-		DATA_BLOCK_CONSTR(double, Double)
-		DATA_BLOCK_CONSTR(long double, LongDouble)
+		template <typename T>
+		inline DataBlock(const T& data) :
+			Data((void*)&data), Size(sizeof(T)), Type(MemoryChunkType::Fundamental)
+		{
+			static_assert(std::is_fundamental<T>());
+		}
 
 		DataBlock(const std::string& data) :
-			DataBlock((void*)&data, (uint32_t)-1, MemoryChunkType::StdString) {}
+			Data((void*)&data), Size((uint32_t)-1), Type(MemoryChunkType::StdString)
+		{
+		}
 
 	public:
 		void* Data;
 		MemoryChunkType Type;
 		uint32_t Size;
-
-	private:
-		DataBlock(void* data, uint32_t size, MemoryChunkType type) :
-			Data(data),
-			Type(type),
-			Size(size)
-		{
-		}
 	};
 
 	struct MemoryChunk
@@ -82,53 +54,48 @@ namespace Hazel
 	{
 	public:
 		template <uint32_t MaxSize>
-		MemoryBuffer<MaxSize> Serialize() const
+		MemoryBuffer<MaxSize>& Serialize(MemoryBuffer<MaxSize>& buffer) const
 		{
-			MemoryBuffer<MaxSize> buffer;
-
 			for (uint32_t i = 0; i < NumVars; ++i)
 			{
 				void* data = (MemoryType*)this + m_Layout[i].Offset;
 
 				switch (m_Layout[i].Type)
 				{
-					case MemoryChunkType::Char:
-					case MemoryChunkType::Short:
-					case MemoryChunkType::Int:
-					case MemoryChunkType::Long:
-					case MemoryChunkType::LongLong:
-					case MemoryChunkType::UChar:
-					case MemoryChunkType::UShort:
-					case MemoryChunkType::UInt:
-					case MemoryChunkType::ULong:
-					case MemoryChunkType::ULongLong:
-					case MemoryChunkType::Float:
-					case MemoryChunkType::Double:
-					case MemoryChunkType::LongDouble:
-					case MemoryChunkType::PtrDiff:
-					{
-						buffer.Push(data, m_Layout[i].Size);
-						break;
-					}
+				case MemoryChunkType::Fundamental:
+				{
+					buffer.Push(data, m_Layout[i].Size);
+					break;
+				}
 
-					case MemoryChunkType::StdString:
-					{
-						buffer.Push(*((std::string*)data));
-						break;
-					}
+				case MemoryChunkType::StdString:
+				{
+					buffer.Push(*((std::string*)data));
+					break;
+				}
 
-					default:
-						HZ_CORE_ASSERT(false, "Unkown memory chunk type!");
+				default:
+					HZ_CORE_ASSERT(false, "Unkown memory chunk type!");
 				}
 			}
 
-			HZ_CORE_INFO("Serialized {0} bytes with remaining headroom of {1} bytes", buffer.GetPointer(), buffer.GetHeadroom());
+			if (buffer.GetHeadroom() > 32)
+				HZ_CORE_WARN("Serialized {0} bytes with remaining headroom of {1} bytes", buffer.GetPointer(), buffer.GetHeadroom());
 
 			return buffer;
 		}
 
-		void Deserialize(MemoryType* stream) const
+		template <uint32_t MaxSize>
+		inline MemoryBuffer<MaxSize> Serialize() const
 		{
+			MemoryBuffer<MaxSize> buffer;
+			Serialize(buffer);
+			return buffer;
+		}
+
+		ptrdiff_t Deserialize(const MemoryType* stream) const
+		{
+			MemoryType* pointer = (MemoryType*)stream;
 			const MemoryType* begin = (MemoryType*)this;
 
 			for (uint32_t i = 0; i < NumVars; ++i)
@@ -137,35 +104,22 @@ namespace Hazel
 
 				switch (m_Layout[i].Type)
 				{
-					case MemoryChunkType::Char:
-					case MemoryChunkType::Short:
-					case MemoryChunkType::Int:
-					case MemoryChunkType::Long:
-					case MemoryChunkType::LongLong:
-					case MemoryChunkType::UChar:
-					case MemoryChunkType::UShort:
-					case MemoryChunkType::UInt:
-					case MemoryChunkType::ULong:
-					case MemoryChunkType::ULongLong:
-					case MemoryChunkType::Float:
-					case MemoryChunkType::Double:
-					case MemoryChunkType::LongDouble:
-					case MemoryChunkType::PtrDiff:
+					case MemoryChunkType::Fundamental:
 					{
-						memcpy(data, stream, m_Layout[i].Size);
+						memcpy(data, pointer, m_Layout[i].Size);
 						break;
 					}
 
 					case MemoryChunkType::StdString:
 					{
 						// Read string length
-						const StringLenType len = *((StringLenType*)stream);
+						const StringLenType len = *((StringLenType*)pointer);
 
 						// Reserve space in string
 						std::string& s = *((std::string*)data);
 						s.resize(len);
 
-						memcpy((void*)s.data(), stream + sizeof(len), len);
+						memcpy((void*)s.data(), pointer + sizeof(len), len);
 
 						break;
 					}
@@ -174,25 +128,26 @@ namespace Hazel
 						HZ_CORE_ASSERT(false, "Unkown memory chunk type!");
 				}
 
-				stream += m_Layout[i].Size;
+				pointer += m_Layout[i].Size;
 			}
+
+			return (ptrdiff_t)pointer - (ptrdiff_t)stream;
 		}
 
 	protected:
 		Serializable() = delete;
-		Serializable(const Serializable&) = delete;
+		//Serializable(const Serializable&) = delete;
 		Serializable(const Serializable&&) = delete;
 
 		template <typename ...Args>
-		inline Serializable(Args&& ...args) { HelpMe({ args... }); }
+		Serializable(Args&& ...args) { HelpMe({ args... }); }
 
 	private:
-		template <uint32_t NumVars2>
-		void HelpMe(const DataBlock(&layout)[NumVars2])
+		template <uint32_t NumArgs>
+		inline void HelpMe(const DataBlock(&layout)[NumArgs])
 		{
 			// Assert number of arguments
-			//HZ_COMPILE_ASSERT(NumVars == NumVars2);
-			HZ_CORE_ASSERT(NumVars == NumVars2, "Number of variables do not match!");
+			static_assert(NumVars == NumArgs, "Number of variables does not match!");
 
 			// Compute offsets
 			for (uint32_t i = 0; i < NumVars; i++)
