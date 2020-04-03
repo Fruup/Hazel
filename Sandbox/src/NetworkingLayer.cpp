83d4c743e2897ba96735d845cbabff955756d6a2
Leon Scherer <scherer-leon@web.de>
2020-04-03 10:30:05 +0200

    Sandbox now functional. Move a block around... online!

--- Sandbox/src/NetworkingLayer.cpp
+++ Sandbox/src/NetworkingLayer.cpp
@@ -1,7 +1,5 @@
 #include "NetworkingLayer.h"
 
-#include <Hazel/Events/NetEvent.h>
-#include <Hazel/Events/KeyEvent.h>
 #include <imgui/imgui.h>
 
 NetworkingLayer::NetworkingLayer() :
@@ -9,6 +7,8 @@ NetworkingLayer::NetworkingLayer() :
 {
 	Hazel::Networking::SetVersion(1);
 	strcpy_s(m_Address, "127.0.0.1:1234");
+
+	srand(262362346);
 }
 
 void NetworkingLayer::OnAttach()
@@ -30,54 +30,133 @@ void NetworkingLayer::OnUpdate(Hazel::Timestep time)
 	// Render
 	Hazel::Renderer2D::BeginScene(Hazel::OrthographicCameraController(Hazel::Application::Get().GetWindow().GetHeight() / Hazel::Application::Get().GetWindow().GetWidth()).GetCamera());
 
-	Hazel::Renderer2D::DrawQuad(glm::vec2(m_Data.x, m_Data.y), glm::vec2(.05f), glm::vec4(0.2f, 1.0f, 0.2f, 1.0f));
+	const float s = .1f;
+	const float speed = .5f;
+
+	for (auto& p : m_Players)
+	{
+		// Move
+		if (p.input[0]) p.obj.y += speed * time;
+		if (p.input[1]) p.obj.x -= speed * time;
+		if (p.input[2]) p.obj.y -= speed * time;
+		if (p.input[3]) p.obj.x += speed * time;
+
+		// Render
+		Hazel::Renderer2D::DrawQuad(
+			glm::vec2(p.obj.x, p.obj.y) - glm::vec2(.5f * s),
+			glm::vec2(s),
+			p.color);
+	}
 
 	Hazel::Renderer2D::EndScene();
 }
 
 void NetworkingLayer::OnEvent(Hazel::Event& event)
 {
-	if (event.GetEventType() == Hazel::ReceivedNetMessageEvent::GetStaticType())
+	if (Hazel::Networking::IsConnected())
 	{
-		auto e = dynamic_cast<Hazel::ReceivedNetMessageEvent*>(&event);
-		m_Data.Deserialize((Hazel::MemoryType*)e->GetPacket()->GetData());
+		Hazel::EventDispatcher d(event);
+
+		d.Dispatch<Hazel::ReceivedNetMessageEvent>(HZ_BIND_EVENT_FN(NetworkingLayer::MsgReceived));
+		d.Dispatch<Hazel::PeerConnectedEvent>(HZ_BIND_EVENT_FN(NetworkingLayer::OnPeerConnected));
+		d.Dispatch<Hazel::PeerDisconnectedEvent>(HZ_BIND_EVENT_FN(NetworkingLayer::OnPeerDisconnected));
+
+		d.Dispatch<Hazel::KeyPressedEvent>(HZ_BIND_EVENT_FN(NetworkingLayer::KeyDown));
+		d.Dispatch<Hazel::KeyReleasedEvent>(HZ_BIND_EVENT_FN(NetworkingLayer::KeyUp));
 	}
+}
 
-	if (Hazel::Networking::IsConnected() && event.GetEventType() == Hazel::MouseMovedEvent::GetStaticType())
+bool NetworkingLayer::OnPeerConnected(Hazel::PeerConnectedEvent& e)
+{
+	m_Players.push_back(Player(e.GetPeer().Id));
+
+	return false;
+}
+
+bool NetworkingLayer::OnPeerDisconnected(Hazel::PeerDisconnectedEvent& e)
+{
+	for (auto it = m_Players.begin(); it != m_Players.end(); ++it)
 	{
-		//if (Hazel::Networking::IsServer())
+		if (it->peerId == e.GetPeerId())
 		{
-			auto e = dynamic_cast<Hazel::MouseMovedEvent*>(&event);
-			const float w = Hazel::Application::Get().GetWindow().GetWidth();
-			const float h = Hazel::Application::Get().GetWindow().GetHeight();
-
-			float x = e->GetX();
-			float y = e->GetY();
-
-			if (x > 0 && x < w &&
-				y > 0 && y < h)
-			{
-				SomeData data;
-				data.x = 2.0f * (x - 150.0f) / w;
-				data.y = 2.0f * (y - 150.0f) / h;
-
-				// Send message
-				Hazel::NetPacket<16> packet(42, Hazel::ClientId::All);
-				packet.Push(data);
-
-				Hazel::Networking::QueuePacket(packet);
-			}
+			m_Players.erase(it);
+			return false;
 		}
 	}
+
+	return false;
+}
+
+bool NetworkingLayer::MsgReceived(Hazel::ReceivedNetMessageEvent& e)
+{
+	auto packet = e.GetPacket();
+
+	if (packet->GetType() == 0)
+	{
+		Player& player = FindPlayerByPeerId(e.GetPacket()->GetSender());
+
+		for (int i = 0; i < 4; i++)
+			packet->Read(&player.input[i]);
+		packet->Read(player.obj);
+	}
+
+	return false;
+}
+
+void NetworkingLayer::SendMsg()
+{
+	Hazel::NetPacket<32> packet(0, Hazel::PeerId::All);
+
+	const Player& player = FindThisPlayer();
+
+	for (int i = 0; i < 4; ++i)
+		packet.Write(player.input[i]);
+	packet.Write(player.obj);
+
+	Hazel::Networking::QueuePacket(packet);
+}
+
+bool NetworkingLayer::KeyDown(Hazel::KeyPressedEvent& e)
+{
+	auto input = FindThisPlayer().input;
+
+	switch (e.GetKeyCode())
+	{
+		case Hazel::KeyCode::W: input[0] = true; break;
+		case Hazel::KeyCode::A: input[1] = true; break;
+		case Hazel::KeyCode::S: input[2] = true; break;
+		case Hazel::KeyCode::D: input[3] = true; break;
+		default:
+			return false;
+	}
+
+	SendMsg();
+
+	return false;
+}
+
+bool NetworkingLayer::KeyUp(Hazel::KeyReleasedEvent& e)
+{
+	auto input = FindThisPlayer().input;
+
+	switch (e.GetKeyCode())
+	{
+		case Hazel::KeyCode::W: input[0] = false; break;
+		case Hazel::KeyCode::A: input[1] = false; break;
+		case Hazel::KeyCode::S: input[2] = false; break;
+		case Hazel::KeyCode::D: input[3] = false; break;
+		default:
+			return false;
+	}
+
+	SendMsg();
+
+	return false;
 }
 
 void NetworkingLayer::OnImGuiRender()
 {
-	if (Hazel::Networking::IsConnected())
-	{
-
-	}
-	else
+	if (!Hazel::Networking::IsConnected())
 	{
 		ImGui::DockSpaceOverViewport();
 
--- Sandbox/src/NetworkingLayer.h
+++ Sandbox/src/NetworkingLayer.h
@@ -4,11 +4,36 @@
 
 #include <Hazel/Core/Serialize.h>
 
-struct SomeData : public Hazel::Serializable<2>
+#include "Hazel/Events/NetEvent.h"
+#include "Hazel/Events/KeyEvent.h"
+
+struct GameObject : public Hazel::Serializable<GameObject>
 {
-	SomeData() : Serializable(x, y) {}
+	GameObject() : Serializable(x, y) {}
 
 	float x = 0.0f, y = 0.0f;
+};
+
+struct Player
+{
+	bool input[4];
+	GameObject obj;
+	Hazel::PeerId::Type peerId = Hazel::PeerId::Invalid;
+	glm::vec4 color;
+
+	Player(Hazel::PeerId::Type id) :
+		input(),
+		obj(),
+		peerId(id),
+		color(
+			(float)(rand() % 100) * .01f,
+			(float)(rand() % 100) * .01f,
+			(float)(rand() % 100) * .01f,
+			1.0f)
+	{
+		for (int i = 0; i < 4; ++i)
+			input[i] = false;
+	}
 };
 
 class NetworkingLayer : public Hazel::Layer
@@ -26,7 +51,30 @@ public:
 	void OnImGuiRender() override;
 
 private:
+	void SendMsg();
+
+	bool MsgReceived(Hazel::ReceivedNetMessageEvent& e);
+	bool OnPeerConnected(Hazel::PeerConnectedEvent& e);
+	bool OnPeerDisconnected(Hazel::PeerDisconnectedEvent& e);
+	bool KeyDown(Hazel::KeyPressedEvent& e);
+	bool KeyUp(Hazel::KeyReleasedEvent& e);
+
+	Player& FindPlayerByPeerId(Hazel::PeerId::Type id)
+	{
+		for (auto& p : m_Players)
+			if (p.peerId == id)
+				return p;
+
+		assert(false);
+		return Player(0);
+	}
+
+	inline Player& FindThisPlayer()
+	{
+		return FindPlayerByPeerId(Hazel::Networking::GetThisPeerId());
+	}
+
 	char m_Address[32];
 
-	SomeData m_Data;
+	std::vector<Player> m_Players;
 };
